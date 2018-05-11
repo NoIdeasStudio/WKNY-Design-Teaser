ac05982218c4b3b4adb6da1e336f6ec0cd0a553f
Sam Kilgus <samkilgus@gmail.com>
2018-05-11 10:01:08 -0400

    code cleanup/prep

--- js/slideshow.js
+++ js/slideshow.js
@@ -5,25 +5,25 @@ function carousel() {
     var i;
     var x = document.getElementsByClassName("slides");
     for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
+       x[i].style.display = "none";
     }
     myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
     setTimeout(carousel, 150);
 }
 
 // var myIndex = 0;
 // carousel2();
-
+//
 // function carousel2() {
 //     var i;
 //     var x = document.getElementsByClassName("slides-slow");
 //     for (i = 0; i < x.length; i++) {
-//        x[i].style.display = "none";  
+//        x[i].style.display = "none";
 //     }
 //     myIndex++;
-//     if (myIndex > x.length) {myIndex = 1}    
-//     x[myIndex-1].style.display = "block";  
+//     if (myIndex > x.length) {myIndex = 1}
+//     x[myIndex-1].style.display = "block";
 //     setTimeout(carousel2, 1003);
 // }
